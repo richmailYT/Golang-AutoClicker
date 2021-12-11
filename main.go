612b8b672e8ddadc077b08cfea5dbcadb350f9ea
richmail <richmail@richmails-iMac.local>
2021-12-11 18:38:29 -0500

    cleaner code

--- main.go
+++ main.go
@@ -13,14 +13,17 @@ import (
 func main() {
 	//delay := flag.Int("delay", 10, "delay between clicks in miliseconds")
 	//flag.Parse()
+
+	//commandline args
 	var button string = os.Args[1]
 	delay, err := strconv.Atoi(os.Args[2])
 	if err == nil {
 		fmt.Printf("%v", err)
 	}
 	var forever bool = true
-	//var i uint64 = 0
-	//i < 99999
+	var i uint64 = 0
+	i++
+	//i < 9999999
 	for forever {
 		x, y := robotgo.GetMousePos()
 		//if the user isn't trying to quit
